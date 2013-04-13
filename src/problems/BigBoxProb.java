eca1bb86ccdae50b38265b0a8f02211c4f5a55ce
Samuel Kim <samuelkim@college.harvard.edu>
2013-04-13 14:09:30 -0400

    BigBox

--- src/problems/BigBoxProb.java
+++ src/problems/BigBoxProb.java
@@ -1,9 +1,23 @@
 package problems;
 
+import java.util.ArrayList;
+
 import solutions.Solution;
 
 public class BigBoxProb extends OptimizationProblem {
 
+	final double boxW, boxL;
+	
+	public BigBoxProb(double width, double length) {
+		this.boxW = width;
+		this.boxL = length;
+	}
+	
+	public BigBoxProb() {
+		this.boxW = 10;
+		this.boxL = 20;
+	}
+	
 	@Override
 	public int getNumVar() {
 		// TODO Auto-generated method stub
@@ -12,8 +26,16 @@ public class BigBoxProb extends OptimizationProblem {
 
 	@Override
 	public double fitness(Solution s) {
+		ArrayList<Double> coefs = s.getCoefs();
+        double volume = coefs.get(0) * coefs.get(1);
+        
+        return area;
+	}
+
+	@Override
+	public boolean contraints(Solution s) {
 		// TODO Auto-generated method stub
-		return 0;
+		return false;
 	}
 
 }
