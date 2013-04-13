daea02e39910da9dfbb5cc2722d0d71694f519b3
Nick Merrill <nicholas.e.merrill@gmail.com>
2013-04-13 12:56:37 -0400

    Using CSSolution's in CSSolutionSet

--- src/solutions/CSSolutionSet.java
+++ src/solutions/CSSolutionSet.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class CSSolutionSet implements SolutionSet {
-    private ArrayList<Solution> solutions;
+    private ArrayList<CSSolution> solutions;
     private Random rand;
     private int numNests;
     private int numVars;
@@ -13,13 +13,13 @@ public class CSSolutionSet implements SolutionSet {
         this.numNests = numNests;
         this.numVars  = numVars;
         this.rand = new Random();
-        this.solutions = new ArrayList<Solution>();
+        this.solutions = new ArrayList<CSSolution>();
         for (int i = 0; i < numNests; i++) {
             this.solutions.set(i, new CSSolution());
         }
     }
     
-    public Solution getRandSol() {
+    public CSSolution getRandSol() {
         return solutions.get(rand.nextInt(this.numNests));
     }
     
