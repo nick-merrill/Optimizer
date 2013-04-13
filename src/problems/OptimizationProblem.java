78cae497c2fc6dcf2a86a4f62f8b46ebf42181c5
Samuel Kim <samuelkim@college.harvard.edu>
2013-04-13 14:04:26 -0400

    Updated OptimizationProblem abstract

--- src/problems/OptimizationProblem.java
+++ src/problems/OptimizationProblem.java
@@ -4,6 +4,6 @@ import solutions.*;
 
 public abstract class OptimizationProblem {
 	public abstract int getNumVar();
-	public abstract float fitness(Solution s);
-
+	public abstract double fitness(Solution s);
+	public abstract boolean contraints(Solution s);
 }
