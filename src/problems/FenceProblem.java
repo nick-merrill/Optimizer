6e3e1f46e0fb1e52a58ed96e78a79c13a580c680
Nick Merrill <nicholas.e.merrill@gmail.com>
2013-04-13 16:31:19 -0400

    Better comments for FenceProblem.

--- src/problems/FenceProblem.java
+++ src/problems/FenceProblem.java
@@ -4,19 +4,27 @@ import java.util.ArrayList;
 
 import solutions.Solution;
 
-
-/* Representation invariant: side1 and side2 are of equal length.
- * Side3 always uses the maximum amount of fence available after
- * side1 and side2 have used fence.
+/**
+ * The objective of the fence problem is to maximize the area
+ * enclosed by a rectangular fence given that the fence only
+ * requires three walls of material (the fourth wall is a river).
+ * Naming convention: side1 and side2 are opposite sides of equal length,
+ * and side3 always uses any remaining fence that side1 and side 2
+ * did not use.
  */
 public class FenceProblem extends OptimizationProblem {
     private double fenceLength;
     
+    /**
+     * Constructs a FenceProblem.
+     */
     public FenceProblem(double fenceLength) {
         this.fenceLength = fenceLength;
     }
     
-    // Return the length of side3, given side1.
+    /**
+     * Returns the length of side3, given side1.
+     */
     private double side3(double side1) {
         return fenceLength - 2*side1;
     }
@@ -27,13 +35,19 @@ public class FenceProblem extends OptimizationProblem {
         return side1 * side3(side1);
     }
     
-    // We maximize for area.
+    /**
+     * Returns the area of the fence, as the objective is
+     * to maximize that.
+     */
     public double fitness(Solution sol) {
         return area(sol);
     }
     
-    /* A solution is within constraints if side lengths
-     * are greater than 0.
+    /**
+     * A solution is within constraints if side lengths
+     * are greater than 0. Note: Ensuring that side3 is
+     * greater than 0 simultaneously ensures that side1
+     * is less than half the fence length.
      */
     public boolean withinConstraints(Solution sol) {
         ArrayList<Double> coefs = sol.getCoefs();
