1354b9c1610019def5b0a89e5a1c4a7c570ccf8c
Crystal Stowell <cstowell@college.harvard.edu>
2013-04-20 20:52:10 -0400

    Random walk sort of.

--- src/problems/OptimizationProblem.java
+++ src/problems/OptimizationProblem.java
@@ -3,7 +3,13 @@ package problems;
 import solutions.Solution;
 
 public abstract class OptimizationProblem {
+	protected double scalingFactor = 1;
+	
 	public abstract int getNumVar();
 	public abstract double fitness(Solution s);
 	public abstract boolean withinConstraints(Solution s);
+	
+	public double getScalingFactor() {
+		return scalingFactor;
+	}
 }
--- src/solutions/Solution.java
+++ src/solutions/Solution.java
@@ -1,8 +1,44 @@
 package solutions;
 
 import java.util.ArrayList;
+import java.util.Random;
 
-public interface Solution {
-    public ArrayList<Double> getCoefs();
-    public void setCoefs(ArrayList<Double> coefs);
+import problems.OptimizationProblem;
+
+public abstract class Solution {
+	
+    protected ArrayList<Double> coefficients;
+    protected int numVars;
+    
+    private Random rand = new Random();
+    
+    public abstract ArrayList<Double> getCoefs();
+    public abstract void setCoefs(ArrayList<Double> coefs);
+    
+    public void randomWalk (OptimizationProblem prob) {
+    	int n = prob.getNumVar();
+    	double distance = rand.nextDouble() * prob.getScalingFactor();
+
+    	ArrayList<Integer> variableIndicies = new ArrayList<Integer>();
+    	for (int i = 0; i < n; i++) {
+    		variableIndicies.set(i, i);
+    	}
+
+    	ArrayList<Double> coefs = this.getCoefs();
+    	for (int i = 0; i < n; i++) {
+    		int index = rand.nextInt(variableIndicies.length());
+    		int coefIndex = variableIndicies.get(index);
+    		
+    		double curCoef = coefs.get(coefIndex);
+    		
+    		double varStep = rand.nextDouble()*distance*2-distance;
+    		
+    		double newCoef = curCoef + varStep;
+    		
+    		coefs.set(coefIndex, newCoef);
+    		
+    		variableIndicies.remove(index);
+    	}
+
+    }
 }
