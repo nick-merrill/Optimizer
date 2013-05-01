43f038f81d1d7d80c7b3bc7d1e99d92b4d9a5480
Samuel Kim <samuelkim@college.harvard.edu>
2013-05-01 15:46:02 -0400

    Factored out tests.

--- src/optimizer.java
+++ src/optimizer.java
@@ -12,44 +12,64 @@ public class optimizer {
 
 	public static void main(String[] args) {
 	    
-//	    LanternaGUI gui = new LanternaGUI();
-//	    
-////		int numEmployees = 20;
-////		NurseSchedProb optProb = new NurseSchedProb(numEmployees);
+		//csTest();
+	    
+		psoTest();
+	    
+    	//nurseSchedTest();
+    	
+	}
+	
+	private void csTest() {
+	    LanternaGUI gui = new LanternaGUI();
+	    
+	    /*
+	    int numEmployees = 20;
+		NurseSchedProb optProb = new NurseSchedProb(numEmployees);
 		CuckooSearchOpt csAlg = new CuckooSearchOpt();
-//		ParticleSwarmOpt psoAlg = new ParticleSwarmOpt();
-////		optAlg.solve(optProb);
-//	    
-//		/*
-//	    double fenceLength = 100.;
-//	    FenceProblem fenceProb = new FenceProblem(fenceLength);
-//	    csAlg.solve(fenceProb);
-//	    csAlg.getSolutions(fenceProb).getMostFitSolution(fenceProb).print();
-//	    
-//	    
-//	    double volume = 100.;
-//	    BoxMinAreaProb boxProb = new BoxMinAreaProb(volume);
-//	    csAlg.solve(boxProb);
-//	    csAlg.getSolutions(boxProb).getMostFitSolution(boxProb).print();
-//	    */
-//	    
-//	    MichaelwiczMinProb michaelwiczProb = new MichaelwiczMinProb();
-//	    csAlg.solve(michaelwiczProb);
-//	    Solution michaelwiczSol = csAlg.getSolutions(michaelwiczProb).getMostFitSolution(michaelwiczProb);
-//	    michaelwiczSol.print();
-//	    System.out.printf("Michaelwicz minimum: %f\n", michaelwiczProb.eval(michaelwiczSol));
-//	    
+		ParticleSwarmOpt psoAlg = new ParticleSwarmOpt();
+		optAlg.solve(optProb);
+		*/
+	    
+		/*
+	    double fenceLength = 100.;
+	    FenceProblem fenceProb = new FenceProblem(fenceLength);
+	    csAlg.solve(fenceProb);
+	    csAlg.getSolutions(fenceProb).getMostFitSolution(fenceProb).print();
+	    
+	    
+	    double volume = 100.;
+	    BoxMinAreaProb boxProb = new BoxMinAreaProb(volume);
+	    csAlg.solve(boxProb);
+	    csAlg.getSolutions(boxProb).getMostFitSolution(boxProb).print();
+	    */
+	    
+	    /*
+	    MichaelwiczMinProb michaelwiczProb = new MichaelwiczMinProb();
+	    csAlg.solve(michaelwiczProb);
+	    Solution michaelwiczSol = csAlg.getSolutions(michaelwiczProb).getMostFitSolution(michaelwiczProb);
+	    michaelwiczSol.print();
+	    System.out.printf("Michaelwicz minimum: %f\n", michaelwiczProb.eval(michaelwiczSol));
+	    */
+	    
 //	    psoAlg.solve(michaelwiczProb);
-////	    ArrayList<PSOSolution> solutions = (ArrayList<PSOSolution>) psoAlg.getSolutions(michaelwiczProb).getSolutions();
-////	    for(PSOSolution sol : solutions) {
-////	    	sol.printAll();
-////	    }
+//	    ArrayList<PSOSolution> solutions = (ArrayList<PSOSolution>) psoAlg.getSolutions(michaelwiczProb).getSolutions();
+//	    for(PSOSolution sol : solutions) {
+//	    	sol.printAll();
+//	    }
 //	    Solution michaelwiczSol2 = psoAlg.getSolutions(michaelwiczProb).getMostFitSolution(michaelwiczProb);
 //	    michaelwiczSol2.print();
-////	    System.out.printf("Michaelwicz minimum: %f\n", michaelwiczProb.eval(michaelwiczSol2));
-	    
-	    
-    	int numEmployees = 3;
+//	    System.out.printf("Michaelwicz minimum: %f\n", michaelwiczProb.eval(michaelwiczSol2));
+	}
+	
+	private void psoTest() {
+		
+	}
+	
+	private void nurseSchedTest() {
+		CuckooSearchOpt csAlg = new CuckooSearchOpt();
+		
+		int numEmployees = 3;
     	int numDays = 3;
     	int numShifts = 4;
     	Integer[] shiftReqArr = new Integer[]{1,1,1,0};
@@ -119,9 +139,6 @@ public class optimizer {
 		} catch (InputException e) {
 			e.printStackTrace();
 		}
-    	
-    	
-    	
 	}
 
 }
